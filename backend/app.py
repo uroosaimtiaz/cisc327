f38ae93643571023b67a040a6a78f40de9a83ea4
Uroosa Imtiaz <uroosaimtiaz@Uroosas-MacBook-Pro.local>
2022-09-25 22:23:44 -0400

    added listing data entity

--- backend/app.py
+++ backend/app.py
@@ -13,3 +13,17 @@ class User(db.Model):
 
     def __repr__(self):
         return '<User %r>' % self.username
+
+# Data Entity for Listing will store the location and description
+# of a specific listing; the images will be stored somewhere else. 
+class Listing(db.Model):
+    street_address = db.Column(db.String(150))
+    postal_code = db.Column(db.String(60))
+    state = db.Column(db.String(80))
+    country = db.Column(db.String(80))
+    house_type = db.Column(db.String(80))
+    rental_cost = db.Column(db.Integer)
+    description = db.Column(db.String(2000))
+
+    def __repr__(self):
+        return '< Listing at %r>' % self.street_address
