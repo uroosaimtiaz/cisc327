2372035914fa6be2613ce40b4c4b581c8ecafb10
Gary <gary.farb23@gmail.com>
2022-10-12 15:32:44 -0400

    adding register function and register tests

--- qbay/models.py
+++ qbay/models.py
@@ -1,16 +1,22 @@
 from qbay import app
 from flask_sqlalchemy import SQLAlchemy
-
+import string
+from email_validator import validate_email, EmailNotValidError
+import uuid
 
 '''
 This file defines data models and related business logics
 '''
 
-
 db = SQLAlchemy(app)
+
+upper_characters = list(string.ascii_uppercase)
+lower_characters = list(string.ascii_lowercase)
+special_characters = list(string.punctuation)
 
 
 class User(db.Model):
+    id = db.Column(db.String)
     username = db.Column(
         db.String(80), nullable=False)
     email = db.Column(
@@ -18,6 +24,12 @@ class User(db.Model):
         primary_key=True)
     password = db.Column(
         db.String(120), nullable=False)
+    billing_address = db.Column(
+        db.String(300))
+    postal_code = db.Column(
+        db.String(300))
+    balance = db.Column(
+        db.Integer)
 
     def __repr__(self):
         return '<User %r>' % self.username
@@ -42,11 +54,52 @@ def register(name, email, password):
     if len(existed) > 0:
         return False
 
+    #  validating the email follows RFC 5322
+    try:
+        validate_email(email).email
+    except EmailNotValidError as errorMsg:
+        print(str(errorMsg))
+        return False
+
+    #  checking for password requirements
+    upper_count = 0
+    lower_count = 0
+    special_count = 0
+    for letter in password:
+        if letter in upper_characters:
+            upper_count += 1
+        if letter in lower_characters:
+            lower_count += 1
+        if letter in special_characters:
+            special_count += 1
+
+    if upper_count <= 0 or lower_count <= 0 or special_count <= 0 or \
+            len(password) < 6:
+        print("not long enough")
+        return False
+
+    # length username
+    if len(name) <= 2 or len(name) >= 20:
+        return False
+
+    # checking username requirements
+    # getting rid of all whitespace to use .isalnum properly
+    temp_name = name.replace(" ", "")
+
+    if name[0] == " " or name[-1] == " " or temp_name.isalnum() is not True:
+        return False
+
     # create a new user
     user = User(username=name, email=email, password=password)
     # add it to the current database session
     db.session.add(user)
-    # actually save the user object
+    # generate random ID for user
+    user.id = str(uuid.uuid1())
+    print("Printing the user id: ", user.id)
+    user.balance = 100
+    print("added 100")
+
+    # actually save the object
     db.session.commit()
 
     return True
