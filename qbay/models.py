86b3567646528388057ce0719da0f57520b0d0d4
Brian Grigore <73979278+Brian-GG@users.noreply.github.com>
2022-10-12 20:28:45 -0400

    added user update function

--- qbay/models.py
+++ qbay/models.py
@@ -147,3 +147,103 @@ def login(email, password):
     if len(valids) != 1:
         return None
     return valids[0]
+
+
+def update_user(old_email, password, new_email, new_name, new_billing_address,
+                new_postal_code):
+    """
+    Update User Profile
+      Parameters:
+        old_email (string):     existing user email
+        password (string):      existing user password
+        new_email (string):     new user email
+        new_name (string):      new username
+        new_billing_address (string):   user billing address
+        new_postal_code (string):   user postal code
+      Returns:
+        The user object if user is able to login and make changes
+        to email, username, billing address, and postal code.
+    """
+    # user must provide correct credentials to login
+    user = login(old_email, password)
+
+    # if user fails to login no changes can be made
+    if user is None:
+        print("User does not exist or login unsuccessful.")
+        return None
+
+    if new_email != " ":
+        # the new email requested cannot already be in use
+        existed = User.query.filter_by(email=new_email).all()
+        if len(existed) > 0:
+            print("This email already is taken.")
+            return None
+
+        # the new email requested must follow RFC 5322 style
+        try:
+            validate_email(new_email).email
+        except EmailNotValidError as errorMsg:
+            print(str(errorMsg))
+            return None
+
+        # new user email is updated
+        user.email = new_email
+
+    # new username must be between 2 and 20 characters long
+    if 20 <= len(new_name) <= 2:
+        print("New username cannot be empty or incorrect length.")
+        return None
+
+    # remove all spaces in new username to use isalnum()
+    temp_name = new_name.replace(" ", "")
+
+    # new username cannot have a " " as prefix or suffix
+    # new username cannot contain non-alphanumeric characters besides " "
+    if new_name == " ":
+        print("username not updated")
+    elif len(new_name) >= 2 and new_name[0] == " " or new_name[-1] == " " or \
+            temp_name.isalnum() is not True:
+        print("New username cannot contain spaces as prefix or suffix.")
+        return None
+    else:
+        # new username is updated
+        user.username = new_name
+
+    # new user billing address is updated
+    user.billing_address = new_billing_address
+
+    # new postal code but be valid CDN postal code
+    if new_postal_code == " ":
+        print("postal code not changed")
+    elif len(new_postal_code) != 7:
+        print("Postal code must be 7 characters.")
+        return None
+    elif not new_postal_code[0] in upper_characters:
+        print("invalid postal code")
+        return None
+    elif not new_postal_code[1] in "0123456789":
+        print("invalid postal code")
+        return None
+    elif not new_postal_code[2] in upper_characters:
+        print("invalid postal code")
+        return None
+    elif new_postal_code[3] != " ":
+        print("invalid postal code")
+        return None
+    elif not new_postal_code[4] in "0123456789":
+        print("invalid postal code")
+        return None
+    elif not new_postal_code[5] in upper_characters:
+        print("invalid postal code")
+        return None
+    elif not new_postal_code[6] in "0123456789":
+        print("invalid postal code")
+        return None
+
+    else:
+        # new user postal code is updated
+        user.postal_code = new_postal_code
+
+    # all user changes are committed to database
+    db.session.commit()
+    return user
