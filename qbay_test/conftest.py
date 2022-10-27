94e156c2e5a6a98f7074881ffd1c66781d6a9dc7
Brian Grigore <73979278+Brian-GG@users.noreply.github.com>
2022-10-26 23:14:57 -0400

    fixed pep8 errors

--- qbay_test/conftest.py
+++ qbay_test/conftest.py
@@ -9,9 +9,9 @@ This file defines what to do BEFORE running any test cases:
 
 
 def pytest_sessionstart():
-    '''
+    """
     Delete database file if existed. So testing can start fresh.
-    '''
+    """
     print('Setting up environment..')
     db_file = 'db.sqlite'
     if os.path.exists(db_file):
@@ -20,8 +20,8 @@ def pytest_sessionstart():
 
 
 def pytest_sessionfinish():
-    '''
+    """
     Optional function called when testing is done.
     Do nothing for now
-    '''
+    """
     pass
